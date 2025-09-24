b141208b79e46fbde86e5e3560d5add0574f12cf
Saumya Patil <saumyasachinpatil@gmail.com>
2025-09-25 00:24:10 +0530

    init

--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -10,18 +10,4 @@ import { index, pgTableCreator } from "drizzle-orm/pg-core";
  *
  * @see https://orm.drizzle.team/docs/goodies#multi-project-schema
  */
-export const createTable = pgTableCreator((name) => `events-capstone_${name}`);
-
-export const posts = createTable(
-  "post",
-  (d) => ({
-    id: d.integer().primaryKey().generatedByDefaultAsIdentity(),
-    name: d.varchar({ length: 256 }),
-    createdAt: d
-      .timestamp({ withTimezone: true })
-      .default(sql`CURRENT_TIMESTAMP`)
-      .notNull(),
-    updatedAt: d.timestamp({ withTimezone: true }).$onUpdate(() => new Date()),
-  }),
-  (t) => [index("name_idx").on(t.name)],
-);
+export const createTable = pgTableCreator((name) => `events_capstone_${name}`);
